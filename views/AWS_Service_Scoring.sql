e469491288e53a678eab17a0ffc4e46d52cc36d9
gbg3 <31935320+gbg3@users.noreply.github.com>
2026-04-03 15:35:21 -0400

    Updates for version 1

--- views/AWS_Service_Scoring.sql
+++ views/AWS_Service_Scoring.sql
@@ -1,7 +1,8 @@
 SELECT account_id, 
 DATE(billing_period) as billing_period, 
-servicecode, IFNULL(score, gemini_1_5_pro_score) as score, 
+servicecode, 
+score_v1 as score, 
 cost 
 From `Cloud_Elevation_Index.AWS_CUR_Cost_By_Service` 
-join (Select * from `Cloud_Elevation_Index.CEI_SKU_Scoring` where provider = "AWS") on servicecode = service_name 
+join (Select * from `Cloud_Elevation_Index.CEI_Versioned_Scoring` where provider = "AWS") on servicecode = service_name 
 group by 1,2,3,4,5 order by account_id
