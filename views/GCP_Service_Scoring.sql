141f7bf0339cc172d4a80d3f99673b181dbc9882
gbg3 <31935320+gbg3@users.noreply.github.com>
2026-04-03 15:02:12 -0400

    Fixed Spelling mistake

--- views/GCP_Service_Scoring.sql
+++ views/GCP_Service_Scoring.sql
@@ -4,6 +4,6 @@ service.description as servicecode,
 IFNULL(score, gemini_1_5_pro_score) as score, 
 cost, 
 IFNULL((SUM((SELECT SUM(if(c.type !="PROMOTION", CAST(c.amount * 1000000 AS int64), 0)) FROM   UNNEST(credits) c))/1000000),0.0) as credits_applied 
-From `{{GCP BIGQQURY BILLING EXPORT}}` 
+From `{{GCP BIGQUERY BILLING EXPORT}}` 
 join (Select * from `Cloud_Elevation_Index.CEI_SKU_Scoring` where provider = "GCP") on service.description = service_name 
 group by 1,2,3,4,5 order by 1
